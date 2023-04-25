a78315895c716ff4a83ae9ebdc4443d5057b55a1
kamal <kamaluiuxd@gmail.com>
2023-04-25 20:29:54 +0530

    img size height set for mobile

--- src/Component/MovieCard.js
+++ src/Component/MovieCard.js
@@ -9,7 +9,7 @@ const MovieCard = ({ movie }) => {
 	};
 
 	return (
-		<div className="m-4 cursor-pointer" onClick={onMovieClick}>
+		<div className="mx-8 cursor-pointer" onClick={onMovieClick}>
 			<div>
 				<img
 					src={movie.Poster}
@@ -19,7 +19,7 @@ const MovieCard = ({ movie }) => {
 							(e.target.src = "https://movieguide.b-cdn.net/wp-content/uploads/2021/09/Poster-Not-Available.jpg")
 						)
 					}
-					className="h-96 md:w-[25rem] w-[25rem] object-cover object-top rounded-t"
+					className="md:h-96 md:w-[25rem] w-full md:object-cover md:object-top rounded-t"
 				/>
 			</div>
 			<div className="flex justify-between items-center p-3 rounded-b bg-stone-50 children:text-lg children:text-black children:font-semibold">
