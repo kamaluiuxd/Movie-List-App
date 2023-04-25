a47e594fb1d49d89d2a350892a48a881831e96a0
kamal <kamaluiuxd@gmail.com>
2023-04-25 16:02:50 +0530

    v3

--- src/Component/MovieCard.js
+++ src/Component/MovieCard.js
@@ -1,36 +1,17 @@
-import { useMovie } from "../Store/MovieContext";
-import empty from "../assets/logok.png";
-
-const MovieCard = () => {
-	const { movieList } = useMovie();
-	console.log(movieList);
+const MovieCard = ({ movie }) => {
 	return (
-		<div>
-			{movieList ? (
-				movieList.map((movie) => {
-					return (
-						<div className="grid md:grid-cols-4 gap-x-6 my-8 mx-auto md:w-[1240px]">
-							{movie.poster == "N/A" ? (
-								<img src={empty} alt="" />
-							) : (
-								<img
-									src={movie.Poster}
-									alt={movie.Title}
-									className="h-96 w-80 mx-8 object-cover object-top rounded-t"
-								/>
-							)}
-							<div className="flex justify-between items-center p-3 rounded-b bg-stone-50 mx-8 w-80 children:text-xl children:text-black children:font-semibold">
-								<h1 className="truncate w-48 ">{movie.Title}</h1>
-								<h1>{movie.Year}</h1>
-							</div>
-						</div>
-					);
-				})
-			) : (
-				<div className="flex justify-center items-center mt-80">
-					<h1 className="text-2xl md:text-9xl text-opacity-50 text-white text-center ">Search Movie</h1>
-				</div>
-			)}
+		<div className="m-4">
+			<div>
+				<img
+					src={movie.Poster}
+					alt={movie.Title}
+					className="h-96 md:w-80 w-[25rem] object-cover object-top rounded-t"
+				/>
+			</div>
+			<div className="flex justify-between items-center p-3 rounded-b bg-stone-50 children:text-xl children:text-black children:font-semibold">
+				<h1 className="truncate w-48 ">{movie.Title}</h1>
+				<h1>{movie.Year}</h1>
+			</div>
 		</div>
 	);
 };
